f4cc196ffb9fc087e2f813a756555ee282ac5c8d
Gilang R <sinyo101520@gmail.com>
2023-04-22 01:50:42 +0700

    Server config adjustment

--- server.cjs
+++ server.cjs
@@ -4,6 +4,10 @@ const app = express(); // create express app
 // add middleware
 app.use(express.static("dist"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 // start express server on port 3001
 app.listen(3001, () => {
   console.log("server started on port 3001");
